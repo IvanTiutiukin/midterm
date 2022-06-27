3a52ed4c8cf2ce60133eb6b952b2ffeab89c4559
Ivan Tiutiukin <tyutyushkin71@gmail.com>
2022-06-26 23:54:32 -0400

    first

---
+++ .history/Server/Config/db_20220626120259.ts
@@ -0,0 +1,4 @@
+export const LocalURI = "mongodb://127.0.0.1/books229";
+export const RemoteURI = "mongodb+srv://ivan:6uE4jB1M1DvJ6qM1@cluster0.zjmtt.mongodb.net/?retryWrites=true&w=majority"
+export const HostName = "LocalHost";
+export const Secret = "someSecret";
---
+++ .history/Server/Routes/books_20220626162736.ts
@@ -0,0 +1,132 @@
+// modules required for routing
+import express from 'express';
+import { CallbackError } from 'mongoose';
+const router = express.Router();
+export default router;
+
+// define the book model
+import book from '../Models/books';
+
+/* GET books List page. READ */
+router.get('/', (req, res, next) => 
+{
+  // find all books in the books collection
+  book.find( (err, books) => {
+    if (err) {
+      return console.error(err);
+    }
+    else {
+      res.render('books/index', {
+        title: 'Books',
+        page: 'books',
+        books: books
+      });
+    }
+  });
+
+});
+
+//  GET the Book Details page in order to add a new Book
+router.get('/add', (req, res, next) => {
+  
+  res.render('index', { title: 'Add Book', page: 'books/details', book: ' '});
+   
+
+});
+
+// POST process the Book Details page and create a new Book - CREATE
+router.post('/details/add', (req, res, next) => {
+
+  let newBook = new book
+  ({
+    "title": req.body.title,
+    "description": req.body.description,
+    "price": req.body.price,
+    "author": req.body.author,
+    "genre": req.body.genre
+    
+  });
+
+  book.create(newBook, function(err: CallbackError)
+  {
+    if(err)
+    {
+      console.error(err);
+      res.end(err);
+    }
+    
+    res.redirect('/books');
+  })
+
+});
+
+// GET the Book Details page in order to edit an existing Book
+router.get('/details/edit:id', (req, res, next) => {
+  let id = req.params.id;
+
+  // pass the id to the db and read the movie into the edit page
+  book.findById(id, {}, {}, function(err, editBook)
+  {
+    if(err)
+    {
+      console.error(err);
+      res.end(err);
+    }
+
+    // show the edit view with the data
+    res.render('index', { title: 'Edit Book', page: 'books/details', book: editBook })
+  });
+});
+
+// POST - process the information passed from the details form and update the document
+router.post('/details/edit/:id', (req, res, next) => {
+
+  let id = req.params.id;
+
+  // instantiate a new Movie to Edit
+  let updatedBook = new book
+  ({
+    "_id": id,
+    "title": req.body.title,
+    "description": req.body.description,
+    "price": req.body.price,
+    "author": req.body.author,
+    "genre": req.body.genre
+  });
+
+  // update the movie in the database
+  book.updateOne({_id: id}, updatedBook, function(err: CallbackError)
+  {
+    if(err)
+    {
+      console.error(err);
+      res.end(err);
+    }
+
+    // edit was successful -> go to the movie-list page
+    res.redirect('/books');
+  });
+
+});
+
+// GET - process the delete by user id
+router.get('/delete/:id', (req, res, next) => {
+
+  let id = req.params.id;
+
+  // pass the id to the database and delete the movie
+  book.remove({_id: id}, function(err: CallbackError)
+  {
+    if(err)
+    {
+      console.error(err);
+      res.end(err);
+    }
+
+    // delete was successful
+    res.redirect('/books');
+  });
+});
+
+
+//module.exports = router;
